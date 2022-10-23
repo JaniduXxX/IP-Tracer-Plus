97a7cdb882899a47947c257411c5153e23fdd9a3
WRx Tec <107356801+Mrjaniya@users.noreply.github.com>
2022-10-23 14:08:11 +0000

    Update .IP-Tracer.php

--- .IP-Tracer.php
+++ .IP-Tracer.php
@@ -17,7 +17,7 @@ function logo() {
 
 
    \033[01;37m}\033[01;31m----------------------------------------\033[01;37m{
-}\033[01;31m-------------- \033[01;32mTrack IPLocation\033[01;31m --------------\033[01;37m{
+}\033[01;31m-------------- \033[01;32mDetails\033[01;31m --------------\033[01;37m{
    }\033[01;31m----------------------------------------\033[01;37m{
 
 \033[00m
@@ -28,9 +28,9 @@ function About() {
   logo();
   echo <<<EOL
 
-         \033[01;33mTool Name \033[01;37m:- \033[01;36mIP-Tracer
-         \033[01;33mAuthor \033[01;37m:- \033[01;36mRajkumar Dusad
-         \033[01;33mPowered By \033[01;37m:- \033[01;36mAex Software's
+         \033[01;33mTool Name \033[01;37m:- \033[01;36mIP-Tracer-Plus
+         \033[01;33mAuthor \033[01;37m:- \033[01;36mJaniduXxX
+         \033[01;33mPowered By \033[01;37m:- \033[01;36mSLR GANZ
 
  \033[01;33mIP-Tracer\033[01;32m is use \033[01;36mip-api \033[01;32mto retrive any ip address information. Our system will automatically ban any IP addresses doing over 150 requests per minute.\033[00m
 
@@ -44,11 +44,11 @@ function upd() {
   logo();
   echo "\n\033[01;32mUpdating IP-Tracer.........\033[01;37m\n\n";
   sleep(1);
-  system("cd ~/ && git clone https://github.com/rajkumardusad/IP-Tracer.git");
-  system("cd ~/ && sudo git clone https://github.com/rajkumardusad/IP-Tracer.git");
+  system("cd ~/ && git clone  https://github.com/Mrjaniya/IP-Tracer-Plus.git ");
+  system("cd ~/ && sudo  git clone https://github.com/Mrjaniya/IP-Tracer-Plus.git ");
   system("cd ~/IP-Tracer && sh install");
   logo();
-  echo "\n\033[01;32m              IP-Tracer updated !!!\033[01;37m\n";
+  echo "\n\033[01;32m              IP-Tracer-Plus Was updated !!!\033[01;37m\n";
   sleep(1);
   menu();
 }
